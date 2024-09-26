2b6fbb20a4f8f08a55c92452be27f9257ef4fc9c
Romano Iván <romanoivan@industrialdetemperley.edu.ar>
2024-09-26 11:46:13 -0300

    Add files via upload

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -8,6 +8,10 @@
 #include <multiplexedQTR.h>
 #include "BluetoothSerial.h"
 
+//Alita toma mate amargo y dulce mientras esucha a Chaqueño Palavecino
+//Chaqueño Palavecino en realidad no es chaqueño
+// :V Salu2 a la grasa
+
 #define PIN_MR1 26 //primer pin motor derecho
 #define PIN_MR2 27 //segundo pin motor derecho
 #define PIN_ML1 16 //primer pin motor izquierdo
@@ -150,12 +154,264 @@ void DisplayMenu(){
   }
 }
 
-void StartAreaCleanerSection() {
-  
-  // Area cleaner trigger
-  if (current_screen == flags && selected == areaCleaner){}
 
+
+//---------------------------------------------------------------------------------------------   
+
+
+
+int datoBT;
+
+#define QRE_BLACK          3900
+#define SHARP_ATAQUE       1000
+#define SET_BACKWARDS_TIME  350 
+
+#define LOW_SPEED   100
+#define MID_SPEED   150
+#define FULL_SPEED  200
+
+#define INCREASE_SPEED        10 
+#define INCREASE_SPEED_DELAY 500
+
+bool offRoad;
+
+
+
+int sharp_left;
+int sharp_right;
+int sharp_front_right;
+int sharp_front;
+int sharp_front_left;
+
+int qre_right;
+int qre_left;
+int any_qre_front;
+bool qre_front;
+bool qre_back;
+
+int signal_input;
+
+CD74HC4067 my_mux(4, 25, 33, 32); // s0, s1, s2, s3
+
+#define PIN_SIG 34
+
+/*bool QREBlack (int qre) {
+  return qre > 3800;
 }
+if (QREBlack(qre_left) && QREBlack(qre_right))*/
+
+void ReadCleanerSensors() {
+
+  qre_front = false;
+  qre_back = false;
+
+  for (int x = 0; x < 15; x++) {
+    
+    my_mux.channel(x);
+  
+    signal_input = analogRead(PIN_SIG);
+
+    switch (x) {
+
+      case 0:
+      case 1:
+      case 2:
+      case 3:
+      case 4:
+      case 5:
+      case 6:
+      case 7: {
+        any_qre_front = signal_input;
+        if (any_qre_front > QRE_BLACK) {
+          qre_front = true;
+        }
+        break;
+      }
+      case 8: {
+        sharp_right = signal_input;
+        break;
+      }
+      case 9: {
+        sharp_front_right = signal_input;
+        break;
+      }
+      case 10: {
+        sharp_front = signal_input;
+        break;
+      }
+      case 11: {
+        sharp_front_left = signal_input; 
+        break;
+      }
+      case 12: { 
+        sharp_left = signal_input;
+        break;
+      } 
+      case 14: {
+        if (signal_input > QRE_BLACK) {
+          qre_back = true;
+        }
+        break;
+      }
+    } 
+  }
+
+  if (qre_front || qre_back) {
+    offRoad = true;
+  } 
+  else {
+    offRoad = false;
+  }
+}
+
+
+bool object_front;
+
+void Out() {
+
+  ReadCleanerSensors();
+
+  unsigned long time;
+
+
+  if (qre_front) { 
+    motors.Brake();
+    delay (200);
+     
+    time = millis();
+
+    while (millis() < time + SET_BACKWARDS_TIME) {
+      motors.MoveBackwards (MID_SPEED , MID_SPEED);
+      delay (10);
+    }
+
+    ReadCleanerSensors();
+
+    SerialBT.println ("FRONT");
+  }
+
+  if (qre_back) {
+    motors.StayStill();
+
+    ReadCleanerSensors();
+
+    SerialBT.println ("BACK");
+  }
+}
+
+
+
+
+
+void SearchObject() {
+  ReadCleanerSensors();
+  long time_limit;
+
+  for (int i=0 ; i<50 ; i++) {
+    time_limit = millis();
+
+    while (millis() < time_limit + 250) {
+      motors.StayStill(); 
+      delay (10);
+    }
+    while (millis() < time_limit + 500) {
+      motors.TurnRight (0 , MID_SPEED);
+      delay (10);
+    }
+
+    ReadCleanerSensors();
+
+    if (sharp_front) {
+      break;
+    }
+  }
+}
+
+
+long time_totals;
+
+long time_found_left;
+long time_found_right;
+int total_found_left;
+int total_found_right;
+
+bool state_found_left;
+bool state_found_right;
+bool state_offRoad;
+
+void StartAreaCleanerSection() {
+
+  ReadCleanerSensors();
+
+  switch (offRoad) {  
+
+    case true: {
+      Out();
+      break;
+    }
+
+    case false: {
+
+      SerialBT.println (time_totals);
+
+      if (total_found_right > 1) {
+        while (millis() < time_totals + total_found_right) {
+          motors.MoveBackwards (MID_SPEED , MID_SPEED);
+          delay (10);
+        }
+        ReadCleanerSensors();
+      }
+
+      if ((sharp_front | sharp_front_left | sharp_front_right) > SHARP_ATAQUE) {
+
+        object_front = true;
+
+        motors.MoveForward (255 , 255);
+        delay (70);
+
+        for (int i=LOW_SPEED ; i<=MID_SPEED ; i+=INCREASE_SPEED) {
+          
+          motors.MoveForward (i , i);
+          delay (INCREASE_SPEED_DELAY);
+
+          ReadCleanerSensors();
+
+          if (offRoad) {
+            break;
+          }
+
+          delay (10);
+        }
+
+        if (sharp_right > SHARP_ATAQUE) {
+          state_found_right = true;
+          time_found_right = millis();
+        }
+
+        ReadCleanerSensors();
+      } 
+      
+      else {
+        SearchObject();
+      }
+
+      
+      if (offRoad) {
+        time_totals = millis();
+        
+        if (state_found_right) {
+          total_found_right = millis() - time_found_right;
+        }
+      }
+      break;
+    }
+  }
+}
+
+
+
+
+//---------------------------------------------------------------------------------------------
 
 #define PIN_LED 23
 
@@ -186,5 +442,7 @@ void setup(){
 
 void loop() {
   DisplayMenu();
-  StartAreaCleanerSection();    
+  if (current_screen == flags && selected == areaCleaner){
+    StartAreaCleanerSection(); 
+  }
 } 
