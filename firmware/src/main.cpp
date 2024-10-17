63586e12626b724b8f9dfa1f5c06c0c5dc20122a
lazzattiL <lazzattilucas06@gmail.com>
2024-10-17 11:11:32 -0300

    Configurando rama para la modalidad Srinter

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -30,135 +30,6 @@ MotorPair motors(PIN_MR1, PIN_MR2, CHANNEL_MR1, CHANNEL_MR2, PIN_ML1, PIN_ML2,
 
 BluetoothSerial SerialBT;
 
-/* End of global section
---------------------------------------------------------------------------*/
-/* Menu section */
-
-// Define display size in pixels
-#define SCREEN_WIDTH 128
-#define SCREEN_HEIGHT 64
-#define OLED_RESET -1
-
-// Define display buttons 
-#define PIN_SELECT 10 //18
-#define PIN_DOWN 5 //5
-
-#define SPRINTER_SCREEN_MARGIN_X 35
-#define SPRINTER_SCREEN_MARGIN_Y 0
-#define SPRINTER_SCREEN_WIDTH 60
-#define SPRINTER_SCREEN_HEIGHT 64
-
-#define CLEANER_SCREEN_MARGIN_X 0
-#define CLEANER_SCREEN_MARGIN_Y 0
-#define CLEANER_SCREEN_WIDTH 128
-#define CLEANER_SCREEN_HEIGHT 64
-
-#define FIRST_SAFETY_TIMEOUT 3000
-#define SECOND_SAFETY_TIMEOUT 2000
-
-#define NUM_MODALITIES 3
-#define MAX_ITEM_LENGTH 20
-
-Adafruit_SH1106 display(OLED_RESET);
-U8G2_FOR_ADAFRUIT_GFX u8g2_for_adafruit_gfx;
-
-ProgressBar displayPB(display);
-
-// Prints a menu
-void DisplayMenu(){
-  if (current_screen == selection) {
-    display.clearDisplay();
-
-    // Prints previous item name
-    u8g2_for_adafruit_gfx.setFontMode(1);
-    u8g2_for_adafruit_gfx.setFontDirection(0);
-    u8g2_for_adafruit_gfx.setForegroundColor(WHITE);
-    u8g2_for_adafruit_gfx.setFont(u8g2_font_7x14_mf);
-    u8g2_for_adafruit_gfx.setCursor(25, 15);
-    u8g2_for_adafruit_gfx.print(F(menu_items [previous]));
-
-    // Prints selected item name
-    u8g2_for_adafruit_gfx.setFontMode(1);
-    u8g2_for_adafruit_gfx.setFontDirection(0);
-    u8g2_for_adafruit_gfx.setForegroundColor(WHITE);
-    u8g2_for_adafruit_gfx.setFont(u8g2_font_7x14B_mf);
-    u8g2_for_adafruit_gfx.setCursor(30, 37);
-    u8g2_for_adafruit_gfx.print(F(menu_items [selected]));
-  
-    // Prints next item name
-    u8g2_for_adafruit_gfx.setFontMode(1);
-    u8g2_for_adafruit_gfx.setFontDirection(0);
-    u8g2_for_adafruit_gfx.setForegroundColor(WHITE);
-    u8g2_for_adafruit_gfx.setFont(u8g2_font_7x14_mf);
-    u8g2_for_adafruit_gfx.setCursor(25, 59);
-    u8g2_for_adafruit_gfx.print(F(menu_items [next]));
-    
-    // Prints selection frame
-    display.drawXBitmap ( 0, 22, epd_bitmap_selected_background, 128, 20, WHITE);
-    
-    // Prints previous item icon
-    display.drawXBitmap ( 4, 2, bitmap_icons[previous], 16, 16, WHITE);
-
-    // Prints selected item icon
-    display.drawXBitmap ( 4, 24, bitmap_icons[selected], 16, 16, WHITE);
-
-    // Prints next item icon
-    display.drawXBitmap ( 4, 46, bitmap_icons[next], 16, 16, WHITE);
-
-    display.display();
-  } 
-  else if (current_screen == modality && selected == sprinter) {
-    displayPB.load(SPRINTER_SCREEN_MARGIN_X, SPRINTER_SCREEN_MARGIN_Y, SPRINTER_SCREEN_WIDTH, SPRINTER_SCREEN_HEIGHT, FIRST_SAFETY_TIMEOUT);
-    displayPB.unload(SPRINTER_SCREEN_MARGIN_X, SPRINTER_SCREEN_MARGIN_Y, SPRINTER_SCREEN_WIDTH, SPRINTER_SCREEN_HEIGHT, SECOND_SAFETY_TIMEOUT);
-
-    current_screen = flags;
-  }
-  else if (current_screen == modality && selected == areaCleaner) {
-    displayPB.load(CLEANER_SCREEN_MARGIN_X, CLEANER_SCREEN_MARGIN_Y, CLEANER_SCREEN_WIDTH, CLEANER_SCREEN_HEIGHT, FIRST_SAFETY_TIMEOUT);   
-    displayPB.unload(CLEANER_SCREEN_MARGIN_X, CLEANER_SCREEN_MARGIN_Y, CLEANER_SCREEN_WIDTH, CLEANER_SCREEN_HEIGHT, SECOND_SAFETY_TIMEOUT);
-
-    current_screen = flags;
-  }
-  else if (current_screen == flags){
-    display.clearDisplay();
-    display.drawXBitmap( 0, 0, epd_bitmap_flag, 128, 64, WHITE);
-    display.display();
-  }
-  else if (current_screen == modality && selected == sumo){
-    /*if (!Ps3.isConnected()) {
-      display.clearDisplay();
-      display.drawXBitmap( 0, 0, bitmap_screens[selected], 128, 64, WHITE);
-      display.display();
-
-      delay(250);
-
-      display.clearDisplay();
-      display.display();
-
-      delay(250);
-    } else {*/
-      display.clearDisplay();
-      display.drawXBitmap( 0, 0, bitmap_screens[selected], 128, 64, WHITE);
-      display.display();
-    //}
-  }
-  else if (current_screen == modality && selected == sprinter){
-    display.clearDisplay();
-    display.drawXBitmap( 0, 0, bitmap_screens[selected], 128, 64, WHITE);
-    display.display();
-  }
-
-  UpdateScreenStatus();
-
-  if (current_screen == selection){  
-    motors.StayStill();
-  }
-}
-
-/* End of menu section
---------------------------------------------------------------------------*/
-/* Sprinter section */
-
 multiplexedQTR qtr;
 
 const uint8_t SensorCount = 8;
@@ -299,7 +170,6 @@ void FloatToString (float value, char* buffer, int bufferSize) {
 
 // Prints SerialBT menu
 void DisplayMenuBT(){
-  
   for (int i = 0; i < 10; i++) {    
     SerialBT.println("");
   }
@@ -432,46 +302,6 @@ void StartTelemetry(){
   }
 }
 
-/* End of SerialBT section
---------------------------------------------------------------------------*/
-/* Area cleaner section */
-
-/* End of area cleaner section
---------------------------------------------------------------------------*/
-/* Sumo section */
-
-/*void ProcessGamepad() {
-  // Variables to store the input of each stick
-  int rightTriggerValue = (Ps3.event.analog_changed.button.r2);  //Left stick  - y axis - forward/backward car movement
-  int leftTriggerValue = (Ps3.event.analog_changed.button.l2);  //Right stick - x axis - left/right car movement
-
-  // Mapping of each stick input to 8bit 
-  int forwardSpeed = map(rightTriggerValue, 0, 127, 0, 255);
-  int backwardsSpeed = map(leftTriggerValue, 0, 127, 0, 255);
-
-  if (rightTriggerValue > 0) {
-    motorRight.MoveForward(forwardSpeed);
-    motorLeft.MoveForward(forwardSpeed);
-  }
-  else if (leftTriggerValue > 0) {
-    motorRight.MoveBackwards(backwardsSpeed);
-    motorLeft.MoveBackwards(backwardsSpeed);
-  }
-  else {
-    motorRight.StayStill();
-    motorLeft.StayStill();
-  }
-}
-
-void StartSumoModality() {
-  if(!Ps3.isConnected()) {Ps3.begin("00:00:00:00:00:04");} 
-  else if (Ps3.isConnected()) {ProcessGamepad();}
-}*/
-
-/* End of sumo section
---------------------------------------------------------------------------*/
-/* Triggers section*/
-
 void StartModalityTriggers() {
   // Calibration trigger
   if (current_screen == calibration){StartSprinterCalibration();}
@@ -494,10 +324,6 @@ void StartModalityTriggers() {
     }
   }
 }
-
-/* End of triggers section
---------------------------------------------------------------------------*/
-/* Setup and loop section */
 
 #define PIN_LED 23
 
@@ -528,5 +354,5 @@ void setup(){
 
 void loop() {
   DisplayMenu();
-  StartModalityTriggers();    
+  StartModalityTriggers();
 } 
