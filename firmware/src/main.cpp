e5e80909a92b78bd22b0eb5d499ec45278362ea4
Romano Iván <romanoivan@industrialdetemperley.edu.ar>
2024-10-18 11:47:35 -0300

    Add files via upload

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -8,6 +8,7 @@
 #include <multiplexedQTR.h>
 #include "BluetoothSerial.h"
 
+
 #define PIN_MR1 26 //primer pin motor derecho
 #define PIN_MR2 27 //segundo pin motor derecho
 #define PIN_ML1 16 //primer pin motor izquierdo
@@ -152,29 +153,20 @@ void DisplayMenu(){
 
 
 
-//---------------------------------------------------------------------------------------------   
+//:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::.::::::::::::::::::  
 
-//Para los motores usamos una lirería propia
-//motors.MoveForwards (MID_SPEED , MID_SPEED) se indica la dirección y el PWM de cada rueda
-//motors.StayStill() baja a 0 los PWM de cada rueda
-//motors.Brake() pone en corto los drivers
-
-int datoBT;
-
-#define QRE_BLACK          3900
-#define SHARP_ATAQUE       1000
-#define SET_BACKWARDS_TIME  350 
+#define QRE_BLACK              3900
+#define SHARP_ATAQUE           1000
+#define SET_OUT_BACKWARDS_TIME  350 
 
 #define LOW_SPEED   100
 #define MID_SPEED   150
 #define FULL_SPEED  200
 
-#define INCREASE_SPEED        10 
-#define INCREASE_SPEED_DELAY 500
+#define INCREASE_SPEED       0.25 
+#define INCREASE_SPEED_DELAY    1
 
 bool offRoad;
-
-
 
 int sharp_left;
 int sharp_right;
@@ -182,9 +174,9 @@ int sharp_front_right;
 int sharp_front;
 int sharp_front_left;
 
-int any_qre_front;
-bool qre_front;
-bool qre_back;
+int qre_back;
+int qre_left;
+int qre_right;
 
 int signal_input;
 
@@ -192,10 +184,7 @@ CD74HC4067 my_mux(4, 25, 33, 32); // s0, s1, s2, s3
 
 #define PIN_SIG 34
 
-void ReadCleanerSensors() {   //Lectura del multiplexor
-
-  qre_front = false;
-  qre_back = false;
+void ReadCleanerSensors() { 
 
   for (int x = 0; x < 15; x++) {
     
@@ -205,18 +194,16 @@ void ReadCleanerSensors() {   //Lectura del multiplexor
 
     switch (x) {
 
-      case 0: //Tuvimos problemas con la lectura de los qre laterales, entonces empezamos a usar la regleta completa
-      case 1: //Es temporal igual, en el próximo robot esto sería corregido
-      case 2:
-      case 3:
-      case 4:
-      case 5:
-      case 6:
+      case 0: {
+        qre_left = signal_input;
+        SerialBT.print ("LEFT = ");
+        SerialBT.println (signal_input);
+        break;
+      }
       case 7: {
-        any_qre_front = signal_input;
-        if (any_qre_front > QRE_BLACK) {
-          qre_front = true;
-        }
+        qre_right = signal_input;
+        SerialBT.print ("RIGHT = ");
+        SerialBT.println (signal_input); 
         break;
       }
       case 8: {
@@ -240,116 +227,138 @@ void ReadCleanerSensors() {   //Lectura del multiplexor
         break;
       } 
       case 14: {
-        if (signal_input > QRE_BLACK) {
-          qre_back = true;
-        }
+        qre_back = signal_input;                                        /* Anashe */
         break;
       }
     } 
   }
+}
 
-  if (qre_front || qre_back) {    //Para trabajar más cómodo, lo uní a la lectura de los sensores
+
+
+void CheckOffRoad() {
+  ReadCleanerSensors();
+
+  if (qre_left > QRE_BLACK || qre_right > QRE_BLACK || qre_back > QRE_BLACK) {
     offRoad = true;
-  } 
-  else {
+  } else {
     offRoad = false;
   }
 }
 
 
-
-
-void Out() {  //Este void se encarga de vovler a la pista cuando offRoad = true;
-
-  ReadCleanerSensors();
-
-  unsigned long time;
-
-
-  if (qre_front) { 
-    motors.Brake();
-    delay (200);
-     
-    time = millis();
-
-    while (millis() < time + SET_BACKWARDS_TIME) {
-      motors.MoveBackwards (MID_SPEED , MID_SPEED);
-      delay (10);
-    }
-
-    ReadCleanerSensors();
-
-    SerialBT.println ("FRONT");
-  }
-
-  if (qre_back) {
-    
-    time = millis();
-
-    while (millis() < time + SET_BACKWARDS_TIME) {
-      motors.MoveBackwards (FULL_SPEED , FULL_SPEED);
-      delay (10);
-    }
-
-    ReadCleanerSensors();
-
-    SerialBT.println ("BACK");
-  }
-}
-
-
-
-
-
-void SearchObject() {   //Para encotrar objetos cuando ningúno de los sharps ve nada y no registró nada al avanzar
-
-  ReadCleanerSensors();
-  long time_limit;  
-  long time_circle;
-
-  time_circle = millis();
-
-  while (millis() < time_circle + 6000) {  //La idea es que de un círculo completo moviendose de a poco
-  //Por limitaciones de los drivers no puedo hacerlos girar a un PWM menor a 100, por eso los ciclos de acelerar y frenar
-
-    for (int i=0 ; i<50 ; i++) {
-      time_limit = millis();
-
-      while (millis() < time_limit + 250) {
-        motors.StayStill(); 
-        delay (10);
-      }
-      while (millis() < time_limit + 200) { 
-        motors.TurnRight (0 , FULL_SPEED);
-        delay (10);
-      }
-
-      ReadCleanerSensors();
-
-      if (sharp_front > SHARP_ATAQUE) {
-        break;
-      }
-    }
-  }
-}
-
-
-long time_totals;
-
 long time_found_left;
 long time_found_right;
 int total_found_left;
 int total_found_right;
-//Estos "times" son para tomar el tiempo transcurrido desde que vío algún objeto al avanzar y así retroceder hasta quedar a la par
+
 
 bool state_found_left;
 bool state_found_right;
 bool state_offRoad;
+bool object_back = false;
+
+
+
+void Out() { 
+  ReadCleanerSensors();
+
+  unsigned long CurrentTime_OUT;
+    
+  if (qre_left > QRE_BLACK || qre_right > QRE_BLACK) {   
+    CurrentTime_OUT = millis();
+
+    while (millis() < CurrentTime_OUT + SET_OUT_BACKWARDS_TIME) {
+      motors.MoveBackwards (MID_SPEED , MID_SPEED);
+      delay (10);
+    }
+
+    CheckOffRoad();
+
+    SerialBT.println ("FRONT");
+  }
+
+  /*if (qre_left > QRE_BLACK && qre_right > QRE_BLACK && qre_back > QRE_BLACK) {
+    CurrentTime_OUT = millis();
+
+    while (millis() < CurrentTime_OUT + 250) {
+      motors.MoveBackwards (FULL_SPEED , FULL_SPEED);
+      delay (10);
+    }
+
+    CheckOffRoad();
+
+    SerialBT.println ("BACK");
+  }*/
+}
+
+
+
+void OutObjectBack() {
+  ReadCleanerSensors();
+
+  long CurrentTime_Totals;
+
+  CurrentTime_Totals = millis();
+
+        
+  if (state_found_left) { 
+    total_found_left = millis() - time_found_left;
+  }
+  else if (state_found_right) {
+    total_found_right = millis() - time_found_right;
+  }
+
+
+  if (total_found_left < total_found_right) {           
+    while (millis() < CurrentTime_Totals + total_found_left) {  
+      motors.MoveBackwards (MID_SPEED , MID_SPEED); 
+    }
+    CheckOffRoad();
+  }
+  else if (total_found_left > total_found_right) {
+    while (millis() < CurrentTime_Totals + total_found_right) {
+      motors.MoveBackwards (MID_SPEED , MID_SPEED);
+    }
+    CheckOffRoad();
+  }
+}
+
+
+
+void SearchObject() {  
+
+  ReadCleanerSensors();  
+
+  SerialBT.println ("SEARCH");
+    
+  for (int i=0 ; i<50 ; i++) {
+    long CurrentTime_Brake = millis() + 250;
+    long CurrentTime_TurnRight = millis() + 120;
+
+    while (millis() < CurrentTime_TurnRight) { 
+      motors.TurnRight (0 , MID_SPEED);
+      ReadCleanerSensors();
+    }
+    while (millis() < CurrentTime_Brake) {
+      motors.Brake(); 
+      ReadCleanerSensors();
+    }
+
+    ReadCleanerSensors();
+    CheckOffRoad();
+
+    if (sharp_front_left > SHARP_ATAQUE || sharp_front > SHARP_ATAQUE || sharp_front_right > SHARP_ATAQUE) {
+      break;
+    }
+  }
+  
+}
 
 
 void StartAreaCleanerSection() {
-
   ReadCleanerSensors();
+  CheckOffRoad();
 
   switch (offRoad) {  
 
@@ -360,72 +369,32 @@ void StartAreaCleanerSection() {
 
     case false: {
 
-      SerialBT.println (time_totals);
+      if (sharp_front_left > SHARP_ATAQUE || sharp_front > SHARP_ATAQUE || sharp_front_right > SHARP_ATAQUE) {
 
-      if (total_found_left < total_found_right) {            //Al volver offRoad a false, que cumpla con el último resultado
-        while (millis() < time_totals + total_found_left) {  //¿Podría no cumplirse por haber renovado todas las variables al pasar de un case a otro?
-          motors.MoveBackwards (MID_SPEED , MID_SPEED); 
-        }
-        ReadCleanerSensors();
-      }
-      if (total_found_left > total_found_right) {
-        while (millis() < time_totals + total_found_right) {
-          motors.MoveBackwards (MID_SPEED , MID_SPEED);
-          delay (10);
-        }
-        ReadCleanerSensors();
-      }
+        SerialBT.println ("ATTACKING");
 
-      if ((sharp_front | sharp_front_left | sharp_front_right) > SHARP_ATAQUE) {
-
-        motors.MoveForward (255 , 255);
-        delay (70); //Como los motores de clavaban (Los drivers estaban en las últimas) le daba un golpe antes de avanzar
-
-        for (int i=LOW_SPEED ; i<=MID_SPEED ; i+=INCREASE_SPEED) { //Para acelerar paulatinamente
-          
-          motors.MoveForward (i , i);
-          delay (INCREASE_SPEED_DELAY);
-
-          ReadCleanerSensors();
-
-          if (offRoad) {
-            break;
-          }
-
-          delay (10);
-        }
-
+        motors.MoveForward (MID_SPEED , MID_SPEED);
 
         if (sharp_left > SHARP_ATAQUE) {
           state_found_left = true;
+          object_back = true;
           time_found_left = millis();
         }
+
         if (sharp_right > SHARP_ATAQUE) {
           state_found_right = true;
+          object_back = true;
           time_found_right = millis();
-        } //Para comenzar a tomar el tiempo si ve algo a los costados mientras ataca
+        }
 
         ReadCleanerSensors();
+        CheckOffRoad();
       } 
       
       else {
         SearchObject();
       }
 
-      
-      if (offRoad) {
-        time_totals = millis();
-        
-        if (state_found_left) { //Para sacar el total del tiempo transcurrido
-          total_found_left = millis() - time_found_left;
-        }
-        if (state_found_right) {
-          total_found_right = millis() - time_found_right;
-        }
-      }
-      //Esta es la parte a la que más le desconfío. Una vez funcionó, pero no más
-      //A lo que le temo es que cuando offRoad = true, pase directo al void Out() antes de sacar los tiempos totales
-      //En caso de que esto pase, ¿Debería solucionarse pasando este ultimo bloque en el case true:?
       break;
     }
   }
@@ -434,7 +403,7 @@ void StartAreaCleanerSection() {
 
 
 
-//---------------------------------------------------------------------------------------------
+//::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
 #define PIN_LED 23
 
